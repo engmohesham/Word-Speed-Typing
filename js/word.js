dcbd1579cdc46139423467dcbd5830baa5b03849
Mohamed Hesham <68177072+engmohesham@users.noreply.github.com>
2022-09-08 23:31:35 +0200

    Add files via upload

---
+++ js/word.js
@@ -0,0 +1,65 @@
+let words = [
+    {
+        word: "meeting",
+    },
+    {
+        word: "number",
+    },
+    {
+        word: "exchange",
+    },
+    {
+        word: "canvas",
+    },
+    {
+        word: "garden",
+    },
+    {
+        word: "position",
+    },
+    {
+        word: "comfort",
+    },
+    {
+        word: "tongue",
+    },
+    {
+        word: "expansion",
+    },
+    {
+        word: "country",
+    },
+    {
+        word: "group",
+    },
+    {
+        word: "taste",
+    },
+    {
+        word: "store",
+    },
+    {
+        word: "field",
+    },
+    {
+        word: "friend",
+    },
+    {
+        word: "pocket",
+    },
+    {
+        word: "needle",
+    },
+    {
+        word: "expert",
+    },
+    {
+        word: "statement",
+    },
+    {
+        word: "second",
+    },
+    {
+        word: "library",
+    },
+]
